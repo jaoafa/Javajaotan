32be30f1d4fad080d389187578e0baa31b04b2c7
Tomachi[ICHIGO] <lego@aol.jp>
2019-11-04 21:14:35 +0900

    add: alphaコマンド実装

---
+++ src/main/java/com/jaoafa/Javajaotan/Command/Cmd_Alpha.java
@@ -0,0 +1,47 @@
+package com.jaoafa.Javajaotan.Command;
+
+import com.jaoafa.Javajaotan.CommandPremise;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class Cmd_Alpha implements CommandPremise {
+	@Override
+	public void onCommand(JDA jda, Guild guild, MessageChannel channel, Member member,
+			Message message, String[] args) {
+		String msg = String.format(
+				"オ、オオwwwwwwwwオレ%swwwwwwww最近めっちょ%sられてんねんオレwwwwwwww%sとかかけるとめっちょ%sやねんwwwwァァァァァァァwww%sを見下しながら食べる%sは一段とウメェなァァァァwwwwwwww",
+				opt(args, 0, "アルファ"),
+				opt(args, 1, "ふぁぼら"),
+				opt(args, 2, "人気"),
+				opt(args, 3, "エゴサ"),
+				opt(args, 4, "クソアルファ"),
+				opt(args, 5, "エビフィレオ"));
+		channel.sendMessage(msg).queue();
+	}
+
+	@Override
+	public String getDescription() {
+		return "アルファになったオレを発言されたチャンネルに投稿します。「アルファ」・「ふぁぼら」・「人気」・「エゴサ」・「クソアルファ」・「エビフィレオ」を順に引数に指定することで別のテキストに置き換えができます。";
+	}
+
+	@Override
+	public String getUsage() {
+		return "/alpha [アルファ] [ふぁぼら] [人気] [エゴサ] [クソアルファ] [エビフィレオ]";
+	}
+
+	@Override
+	public boolean isjMSOnly() {
+		return false;
+	}
+
+	private String opt(String[] args, int key, String defaultValue) {
+		if (key < 0 || key >= args.length) {
+			return defaultValue;
+		}
+		return args[key];
+	}
+}
