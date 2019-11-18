e0b3657a4d6a64049353ef579156ff7e7b23fa6e
Tomachi[MINT] <lego@aol.jp>
2019-11-18 15:12:49 +0900

    add: TomachiEmojis#emoji-log機能追加

---
+++ src/main/java/com/jaoafa/Javajaotan/Event/Event_TomachiEmojis.java
@@ -0,0 +1,70 @@
+package com.jaoafa.Javajaotan.Event;
+
+import java.time.Instant;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.entities.ListedEmote;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.emote.EmoteAddedEvent;
+import net.dv8tion.jda.api.events.emote.EmoteRemovedEvent;
+import net.dv8tion.jda.api.events.emote.update.EmoteUpdateNameEvent;
+import net.dv8tion.jda.api.hooks.SubscribeEvent;
+
+public class Event_TomachiEmojis {
+	@SubscribeEvent
+	public void onEmoteAddedEvent(EmoteAddedEvent event) {
+		if (event.getGuild().getIdLong() != 645864591775105034L) {
+			return; // Tomachi Emojisのみ
+		}
+		Emote emote = event.getEmote();
+		ListedEmote listemote = event.getGuild().retrieveEmoteById(emote.getIdLong()).complete();
+		User user = listemote.getUser();
+
+		TextChannel emojiLog = event.getGuild().getTextChannelById(645864591775105034L);
+		EmbedBuilder builder = new EmbedBuilder();
+		builder.setTitle(":new:NEW EMOJI : " + emote.getAsMention() + " (`:" + emote.getName() + ":`)");
+		builder.setThumbnail(emote.getImageUrl());
+		builder.setAuthor(user.getAsMention());
+		builder.setTimestamp(Instant.now());
+		emojiLog.sendMessage(builder.build()).queue();
+	}
+
+	@SubscribeEvent
+	public void onEmoteUpdateNameEvent(EmoteUpdateNameEvent event) {
+		if (event.getGuild().getIdLong() != 645864591775105034L) {
+			return; // Tomachi Emojisのみ
+		}
+		Emote emote = event.getEmote();
+		ListedEmote listemote = event.getGuild().retrieveEmoteById(emote.getIdLong()).complete();
+		User user = listemote.getUser();
+
+		TextChannel emojiLog = event.getGuild().getTextChannelById(645864591775105034L);
+		EmbedBuilder builder = new EmbedBuilder();
+		builder.setTitle(":repeat:CHANGE EMOJI : " + emote.getAsMention()
+				+ " (`:" + event.getOldName() + ":` -> `:" + event.getNewName() + ":`)");
+		builder.setThumbnail(emote.getImageUrl());
+		builder.setAuthor(user.getAsMention());
+		builder.setTimestamp(Instant.now());
+		emojiLog.sendMessage(builder.build()).queue();
+	}
+
+	@SubscribeEvent
+	public void onEmoteRemovedEvent(EmoteRemovedEvent event) {
+		if (event.getGuild().getIdLong() != 645864591775105034L) {
+			return; // Tomachi Emojisのみ
+		}
+		Emote emote = event.getEmote();
+		ListedEmote listemote = event.getGuild().retrieveEmoteById(emote.getIdLong()).complete();
+		User user = listemote.getUser();
+
+		TextChannel emojiLog = event.getGuild().getTextChannelById(645864591775105034L);
+		EmbedBuilder builder = new EmbedBuilder();
+		builder.setTitle(":wave:DELETED EMOJI : (`:" + emote.getName() + ":`)");
+		builder.setThumbnail(emote.getImageUrl());
+		builder.setAuthor(user.getAsMention());
+		builder.setTimestamp(Instant.now());
+		emojiLog.sendMessage(builder.build()).queue();
+	}
+}
