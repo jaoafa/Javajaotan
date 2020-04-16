e98b50633aebfcd9cc1788725cfdb77a0407231b
Tomachi (ICHIGO) <lego@aol.jp>
2020-04-17 01:34:11 +0900

    add: playing, watching, listeningコマンド追加

---
+++ src/main/java/com/jaoafa/Javajaotan/Command/Cmd_Watching.java
@@ -0,0 +1,37 @@
+package com.jaoafa.Javajaotan.Command;
+
+import com.jaoafa.Javajaotan.CommandPremise;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+
+public class Cmd_Watching implements CommandPremise {
+	@Override
+	public void onCommand(JDA jda, Guild guild, MessageChannel channel, Member member,
+			Message message, String[] args) {
+		if (args.length == 0) {
+			channel.sendMessage(member.getAsMention() + ", このコマンドを実行するには、1つ以上の引数が必要です。").queue();
+			return;
+		}
+		jda.getPresence().setActivity(Activity.watching(String.join(" ", args)));
+	}
+
+	@Override
+	public String getDescription() {
+		return "jaotanのNowWatchingを更新します。";
+	}
+
+	@Override
+	public String getUsage() {
+		return "/watching <Text>";
+	}
+
+	@Override
+	public boolean isjMSOnly() {
+		return true;
+	}
+}
