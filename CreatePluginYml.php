534790bdccf133beae8934f0845b08614c0daa0e
Tomachi <book000@users.noreply.github.com>
2021-02-03 18:32:10 +0900

    Update CreatePluginYml.php

--- CreatePluginYml.php
+++ CreatePluginYml.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Tokyo");
 define("Version", date("Y.m.d_H.i")); // バージョン
 define("FileLocation", __DIR__ . "/src/main/resources/version");
 
-if (file_exists(dirname(FileLocation))){
+if (!file_exists(dirname(FileLocation))){
     mkdir(dirname(FileLocation), 0777, true);
 }
 
